b4e39227fcc40fa105ffe13b95efb457c2c32ae2
José Quinde <49071271+JoseQ1996@users.noreply.github.com>
2022-07-18 14:40:33 -0500

    navegacion

--- src/app/componentes/nav-bar/nav-bar.component.ts
+++ src/app/componentes/nav-bar/nav-bar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { SesionService } from 'src/app/servicios/sesion.service';
+import { LoginComponent } from '../login/login.component';
+import { PerfilComponent } from '../perfil/perfil.component';
+import { RegistroComponent } from '../registro/registro.component';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,9 +12,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  public sesionActiva: boolean = false;
+
+  constructor(public dialog: MatDialog, private ss: SesionService,) { }
 
   ngOnInit(): void {
+    const usuario = this.ss.obtenerSesion();
+    if (usuario) {
+      this.sesionActiva = true;
+    }
+  }
+
+  public login() {
+    const dialogRef = this.dialog.open(LoginComponent, {
+      width: '400px',
+      height: '380px',
+      panelClass: 'my-dialog',
+      disableClose: false
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  public perfil() {
+    const dialogRef = this.dialog.open(PerfilComponent, {
+      width: '400px',
+      height: '380px',
+      panelClass: 'my-dialog',
+      disableClose: false
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  public register() {
+    const dialogRef = this.dialog.open(RegistroComponent, {
+      width: '400px',
+      height: '430px',
+      panelClass: 'my-dialog',
+      disableClose: false
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  public logout() {
+    setTimeout(() => { 
+      this.ss.cerrarSesion();
+      location.href = '/';
+     }, 1000)
   }
 
 }
